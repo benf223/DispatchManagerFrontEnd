244c3da578f84279b378e6bf4614b96aa3938ee1
Ben <ppj1483@autuni.ac.nz>
2018-08-15 21:13:05 +1200

    Remove old code. Fix the notification values.

--- src/app/truck-slots/truck-slots.component.ts
+++ src/app/truck-slots/truck-slots.component.ts
@@ -174,6 +174,7 @@ export class TruckSlotsComponent implements OnInit {
                 // just overwrite i = 1 and write to i = 2
                 this.releases[1] = movedRelease;
                 this.releases[2] = movedRelease;
+                updated = true;
               }
             } else if (i === 2) {
               // check that i = 1 can be replaced ...
@@ -242,6 +243,7 @@ export class TruckSlotsComponent implements OnInit {
                       });
                     } else {
                       this.releases[2] = movedRelease;
+                      updated = true;
                     }
                   }
                 });
@@ -272,6 +274,7 @@ export class TruckSlotsComponent implements OnInit {
                       });
                     } else {
                       this.releases[2] = movedRelease;
+                      updated = true;
                     }
                   }
                 });
@@ -295,6 +298,7 @@ export class TruckSlotsComponent implements OnInit {
                       });
                     } else {
                       this.releases[0] = movedRelease;
+                      updated = true;
                     }
                   }
                 });
@@ -320,6 +324,7 @@ export class TruckSlotsComponent implements OnInit {
                       });
                     } else {
                       this.releases[0] = movedRelease;
+                      updated = true;
                     }
                   }
                 });
@@ -332,163 +337,12 @@ export class TruckSlotsComponent implements OnInit {
             }
           } else {
             this.releases[i] = movedRelease;
+            updated = true;
           }
         }
 
       }
     }
-
-
-
-    //   if (movedRelease.size === 40) {
-    //     if (this.releases[0].size === 40) {
-    //       // Means that a 40 is already in a slot and will need to be handled or overwritten.
-    //       // Should split the above condition to save time
-    //         if (i === 0) {
-    //           this.openDialog('Replace Data', ['Do you wish to replace: ' + this.releases[1].release +
-    //           ', with: ' + movedRelease.release], null).afterClosed().subscribe((result: string) => {
-    //             if (result === 'a') {
-    //
-    //             } else if (result === 'c') {
-    //
-    //             }
-    //           });
-    //         } else if (i === 1) {
-    //           this.openDialog('Replace Data', ['Do you wish to replace: ' + this.releases[0].release +
-    //           ', with: ' + movedRelease.release], null).afterClosed().subscribe((result: string) => {
-    //             if (result === 'a') {
-    //
-    //             } else if (result === 'c') {
-    //
-    //             }
-    //           });
-    //         } else {
-    //           // Should not be relevant
-    //         }
-    //     } else if (this.releases[1].size === 40) {
-    //       if (i === 0) {
-    //         // Is this correct
-    //         this.openDialog('Replace Data', ['Do you wish to replace: ' + this.releases[0].release +
-    //         ', with: ' + movedRelease.release], null).afterClosed().subscribe((result: string) => {
-    //           if (result === 'a') {
-    //
-    //           } else if (result === 'c') {
-    //
-    //           }
-    //         });
-    //       } else if (i === 1) {
-    //         // Should not be relevant
-    //       } else {
-    //         // Is this correct
-    //         this.openDialog('Replace Data', ['Do you wish to replace: ' + this.releases[2].release +
-    //         ', with: ' + movedRelease.release], null).afterClosed().subscribe((result: string) => {
-    //           if (result === 'a') {
-    //
-    //           } else if (result === 'c') {
-    //
-    //           }
-    //         });
-    //       }
-    //     }
-    //
-    //     // This implementation does not check to see if there is already a 40 in the truck and handle that interaction
-    //     if (i === 2) {
-    //       if (!this.releases[1]) {
-    //         this.releases[1] = movedRelease;
-    //         this.releases[2] = movedRelease;
-    //         updated = true;
-    //       } else {
-    //         if (!this.releases[0]) {
-    //           this.releases[0] = this.releases[1];
-    //           this.releases[1] = movedRelease;
-    //           this.releases[2] = movedRelease;
-    //           updated = true;
-    //         } else {
-    //           this.openDialog('Replace Data',['Do you wish to replace: ' + this.releases[1].release +
-    //           ', with: ' + movedRelease.release, null])
-    //             .afterClosed().subscribe((result: string) => {
-    //             if (result === 'a') {
-    //               this.releases[1] = movedRelease;
-    //               this.releases[2] = movedRelease;
-    //               this.updated.emit(this.truckID);
-    //             } else if (result === 'c') {
-    //               // Do nothing
-    //             }
-    //           });
-    //         }
-    //       }
-    //     } else if (i === 1) {
-    //       if (!this.releases[2]) {
-    //         this.releases[1] = movedRelease;
-    //         this.releases[2] = movedRelease;
-    //         updated = true;
-    //       } else {
-    //         if (!this.releases[0]) {
-    //           this.releases[0] = this.releases[2];
-    //           this.releases[1] = movedRelease;
-    //           this.releases[2] = movedRelease;
-    //           updated = true;
-    //         } else {
-    //           this.openDialog('Replace Data',['Do you wish to replace: ' + this.releases[2].release +
-    //           ', with: ' + movedRelease.release, null])
-    //             .afterClosed().subscribe((result: string) => {
-    //             if (result === 'a') {
-    //               this.releases[1] = movedRelease;
-    //               this.releases[2] = movedRelease;
-    //               this.updated.emit(this.truckID);
-    //             } else if (result === 'c') {
-    //               // Do nothing
-    //             }
-    //           });
-    //         }
-    //       }
-    //     } else if (i === 0) {
-    //       if (!this.releases[1]) {
-    //         this.releases[0] = movedRelease;
-    //         this.releases[1] = movedRelease;
-    //         updated = true;
-    //       } else {
-    //         if (!this.releases[2]) {
-    //           this.releases[2] = this.releases[0];
-    //           this.releases[0] = movedRelease;
-    //           this.releases[1] = movedRelease;
-    //           updated = true;
-    //         } else {
-    //           this.openDialog('Replace Data', ['Do you wish to replace: ' + this.releases[1].release +
-    //           ', with: ' + movedRelease.release, null])
-    //             .afterClosed().subscribe((result: string) => {
-    //             if (result === 'a') {
-    //               this.releases[0] = movedRelease;
-    //               this.releases[1] = movedRelease;
-    //               this.updated.emit(this.truckID);
-    //             } else if (result === 'c') {
-    //               // Do nothing
-    //             }
-    //           });
-    //         }
-    //       }
-    //     }
-    //   } else if (movedRelease.size === 20) {
-    //     if (this.releases[i].size === 40) {
-    //       // Means that there is a 40 in the slot already and will need to find the other part and possibly overwrite
-    //
-    //       if (i === 0) {
-    //         if (this.releases[1].size === 40) {
-    //           // Found the rest of the 40
-    //         }
-    //       } else if (i === 1) {
-    //         if (this.releases[0].size === 40) {
-    //           // Found the rest of the 40
-    //         } else if (this.releases[2].size === 40) {
-    //           // Found the rest of the 40 on the other side
-    //         }
-    //       }
-    //     } else {
-    //       this.releases[i] = movedRelease;
-    //       updated = true;
-    //     }
-    //   }
-    // }
 
     if (updated) {
       this.updated.emit(this.truckID);
